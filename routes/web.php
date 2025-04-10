9fe1b1e762a4baac012e3a578dc140d5964e9465
Ricardo Blanco <odraciraicrag825@gmail.com>
2025-04-10 14:55:44 -0600

    Este es el cambio 2

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,21 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Models\User;
+use App\Models\Post;
 
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/prueba', function () {
+    $id = 1;
+    $title = "Title 4";
+
+    $post =  Post::where('title', $title) -> get();
+
+    $post -> delete();
+
+    //return $post; 
+});
+?>
